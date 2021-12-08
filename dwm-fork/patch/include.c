dc0fafd6ff7af3699d92992a8298acb1dc2d99cd
garfield-spst <94508945+garfield-spst@users.noreply.github.com>
2021-12-08 19:41:54 +0530

    Add files via upload

---
+++ dwm-fork/patch/include.c
@@ -0,0 +1,21 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+
+#include "bar_ltsymbol.c"
+#include "bar_status2d.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+
+/* Other patches */
+#include "fullscreen.c"
+#include "movestack.c"
+#include "pertag.c"
+#include "scratchpad.c"
+#include "swallow.c"
+#include "tab.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
---
+++ dwm-fork/patch/include.h
@@ -0,0 +1,20 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status2d.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+
+/* Other patches */
+#include "fullscreen.h"
+#include "movestack.h"
+#include "pertag.h"
+#include "scratchpad.h"
+#include "swallow.h"
+#include "tab.h"
+/* Layouts */
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
